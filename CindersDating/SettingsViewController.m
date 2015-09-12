aef2ec0d3cb8cbf3e164585d2cdd240289a78bfe
Oscar Villavicencio <odvcencio@gmail.com>
2015-09-12 18:45:47 -0400

    Settings change and also podfile

--- CindersDating/SettingsViewController.m
+++ CindersDating/SettingsViewController.m
@@ -4,22 +4,9 @@
 //
 #import <Parse/Parse.h>
 #import "SettingsViewController.h"
-#define MILES2KM(miles) (ceil(miles*1609.34))
-#define MILES2METERS(mi) ((float)mi*1609.34))
+
 @interface SettingsViewController ()
-@property (weak, nonatomic) IBOutlet UISwitch *PhotoSharing;
-@property (weak, nonatomic) IBOutlet UISwitch *ProfileOn;
-@property (weak, nonatomic) IBOutlet UISegmentedControl *GenderChooser;
 
-@property (weak, nonatomic) IBOutlet UISlider *WithinMiles;
-@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *LoadingOne;
-@property (weak, nonatomic) IBOutlet UIButton *SaveSettings;
-
-@property (weak, nonatomic) IBOutlet UISlider *youngest;
-@property (weak, nonatomic) IBOutlet UISlider *Oldest;
-@property (weak, nonatomic) IBOutlet UILabel *MinAgeShower;
-@property (weak, nonatomic) IBOutlet UILabel *MaxAgeShower;
-@property (weak, nonatomic) IBOutlet UILabel *DistanceShower;
 
 
 @end
@@ -28,13 +15,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    UISlider *slidebar = [[UISlider alloc]init];
-    slidebar.minimumValue = 1;
-    slidebar.maximumValue = 125;
-    PFQuery *query = [PFUser query];
-    [query whereKey:@"gender" equalTo:@"male"]; // find all the men
-    NSArray *men = [query findObjects];
+
 }
 - (IBAction)doneButton:(id)sender {
     
@@ -62,218 +43,11 @@
 
 
 #pragma mark - Helper Function
-- (IBAction)ShowProfile:(id)sender {
-
-
-
-}
-
-
-
-- (IBAction)PhotoSharing:(id)sender {
-
-
-
-}
-
-- (IBAction)DistanceRange:(id)sender {
-    self.DistanceShower.text = [[NSString alloc]initWithFormat:@"%.1f mi", ceil
-                            (self.WithinMiles.value)];
-    float f =self.WithinMiles.value;
-     PFUser *currentUser = [PFUser currentUser];
-    [currentUser setObject:[NSNumber numberWithFloat:f] forKey:@"searchDist"];
-    [currentUser saveInBackground];
-}
-
-
-
-
-- (IBAction)GenderBender:(id)sender {
-    UISegmentedControl * segmentedControl = (UISegmentedControl *)sender;
-     PFUser *currentUser = [PFUser currentUser];
-    if(segmentedControl.selectedSegmentIndex == 0){
-        
-  
-        
-        [currentUser setObject:@YES forKey:@"showMan"];
-        [currentUser setObject:@NO forKey:@"showWoman"];
-        
-        
-//        PFQuery *query0 = [PFUser query];
-//        [query0 whereKey:@"gender" equalTo:@"male"]; // find all the men
-//        NSArray *men = [query0 findObjects];
-//        
-//    
-//        NSLog ([NSString stringWithFormat:@"%lu", (unsigned long)men.count]);
-//        
-        
-        
-        [[PFUser currentUser] saveInBackground];
-    
-    } else if (segmentedControl.selectedSegmentIndex == 1){
-    
-        
-        [currentUser setObject:@YES forKey:@"showWoman"];
-         [currentUser setObject:@NO forKey:@"showMan"];
-        
-         [[PFUser currentUser] saveInBackground];
-        
-        //
-//        PFQuery *query = [PFUser query];
-//        [query whereKey:@"gender" equalTo:@"female"]; // find all the women
-//        NSArray *women = [query findObjects];
-    
-        
-        
-        
-        // NSLog ([NSString stringWithFormat:@"%lu", (unsigned long)women.count]);
-    }
-    
-    else if (segmentedControl.selectedSegmentIndex == 2){
-       
-
-        [currentUser setObject:@YES forKey:@"showWoman"];
-        [currentUser setObject:@YES forKey:@"showMan"];
-        
-         [[PFUser currentUser] saveInBackground];
-        
-        
-        
-        //        PFQuery *query2 = [PFUser query];
-//        [query2 whereKey:@"gender" equalTo:@"female"];
-//        
-//        
-//        PFQuery *query3 = [PFUser query];
-//        [query3 whereKey:@"gender" equalTo:@"male"];
-//        PFQuery *query = [PFQuery orQueryWithSubqueries:@[query3,query2]];
-//        NSArray *both = [query findObjects];
-//        
-//        
-//        
-//        
-//        
-//        
-//        
-//        
-//
-//  NSLog ([NSString stringWithFormat:@"%lu", (unsigned long)both.count]);
-// |  0   |  1   |  2    |  3    |  4   |  5   |
-//  showP  photoS  minAge MaxAge   dist   M/F
- 
-  [[PFUser currentUser] saveInBackground];
-    }
-}
-
-
--(NSMutableArray*)dataGathering{
-    NSMutableArray * dataGatReturn = [[NSMutableArray alloc]init];
-    
-    //////////////////////////////  photo sharing on/off
-    if (self.PhotoSharing) {
-        [dataGatReturn addObject:@"OnPhotoSharing"];
-    } else {
-        [dataGatReturn addObject:@"OffPhotoSharing"];
-    }
-    //////////////////////////////  profile on/off
-    if (self.ProfileOn) {
-        [dataGatReturn addObject:@"OnProfile"];
-    } else {
-        [dataGatReturn addObject:@"OffProfile"];
-    }
-    //////////////////////////////  min age
-
-       // [dataGatReturn addObject:[NSNumber  numberWithInteger:]];
-          [dataGatReturn addObject:@"20"];
-    //////////////////////////////  max age
-
-      //  [dataGatReturn addObject:[NSNumber  numberWithInteger:]];
-           [dataGatReturn addObject:@"27"];
-    ////////////////////////////// distance
-
-      //  [dataGatReturn addObject:[NSNumber  numberWithFloat:[self.]];
-           [dataGatReturn addObject:[NSNumber  numberWithInteger:MILES2KM(100)]];
-    //////////////////////////////  man/woman
-    switch (self.GenderChooser.selectedSegmentIndex) {
-        case 0:
-            [dataGatReturn addObject:@"Man"];
-            break;
-        case 1:
-            [dataGatReturn addObject:@"Woman"];
-            break;
-        default:
-            break;
-    }
-    
-    return dataGatReturn;
-}
-
-
-
-- (IBAction)SaveSettings:(id)sender
 
 
 
 
 
-    {
-
-    
-    
-    
-    }
-
-/*//- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    YelpMapViewController *webVC = [segue destinationViewController];
-    
-   // webVC.range = [[NSString alloc]initWithFormat:@"%f", 1609.34*(self.WithinMiles.value)];
-    //webVC.url = self.url;
-    
-    //////////////////////////////////////////////////////////////////////////////
-   // NSLog(@"%@", webVC.range);
-    NSLog(@" should segue out ");
-}*/
-// When sugueing out enter this code in the save method  [self performSegueWithIdentifier:@"webViewSegue" sender:self];
-
-
-float RoundValue(UISlider * slider) {
-    return roundf(slider.value * 2.0) * 1;
-}
-
-- (IBAction)MinAgeBar:(UISlider *)sender {
-    self.Oldest.minimumValue = sender.value;
-
-    float myValue = RoundValue(sender);
-    
-    
-    self.MinAgeShower.text = [[NSString alloc]initWithFormat:@"%.0f", ceil
-                                (self.youngest.value)];
-    
-    
-    int f =self.youngest.value;
-    PFUser *currentUser = [PFUser currentUser];
-    [currentUser setObject:[NSNumber numberWithInt:f] forKey:@"minAgeToSearch"];
-    [currentUser saveInBackground];
-
-
-}
-
-
-
-
-- (IBAction)MaxAgeBar:(UISlider *)sender {
-    self.youngest.maximumValue = sender.value;
-
-    
-    float myValue = RoundValue(sender);
-    self.MaxAgeShower.text = [[NSString alloc]initWithFormat:@"%.0f", ceil
-                                  (self.Oldest.value)];
-    
-    
-    int f =self.Oldest.value;
-    PFUser *currentUser = [PFUser currentUser];
-    [currentUser setObject:[NSNumber numberWithInt:f] forKey:@"maxAgeToSearch"];
-    [currentUser saveInBackground];
-}
 
 
 
--- Podfile
+++ Podfile
@@ -1,4 +1,8 @@
 platform :ios, '8.0'
+source ‘https://github.com/CocoaPods/Specs.git'
+
+
+pod 'TTRangeSlider'
 pod 'ParseUI', '~> 1.1.3'
 pod 'Parse', '~> 1.7.2'
 pod 'FBSDKCoreKit',  '4.1.0'
@@ -7,5 +11,6 @@ pod 'FBSDKShareKit', ‘4.1.0'
 pod 'ParseFacebookUtilsV4', '~>1.7.2'
 pod 'JSQMessagesViewController'
 pod 'SDWebImage'
+
 
 
